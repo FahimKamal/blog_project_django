827d59cb81ae2c7bb9fca5f754bb82fd25143929
FahimKamal <fahimkamal63@gmail.com>
2020-05-08 11:16:02 +0600

    Templates created, views and urls are set

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,1 +1,28 @@
-# This is a test
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from blog import views
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('about/',views.AboutView.as_view(), name='about'),
+    path('post/<int:pk>',views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
+    path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+]
